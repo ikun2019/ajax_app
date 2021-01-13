a5eed71f8c5bffc060c9edfca7f8f784bb92ea34
ikun <isao.miyagawa.2019@gmail.com>
2021-01-13 23:17:45 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,8 @@ class PostsController < ApplicationController
   # end
   
   def create
-    Post.create(post_params)
-    redirect_to action: :index
+    post = Post.create(post_params, checked: false)
+    render json: { post: post }
   end
 
   def checked
