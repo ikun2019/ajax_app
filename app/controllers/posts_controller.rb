018b49d7b205b440540fce2cb22ce81e59b11f1d
ikun <isao.miyagawa.2019@gmail.com>
2021-01-13 22:19:08 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-    @post = Post.new
-  end
+  # def new
+  #   @post = Post.new
+  # end
   
   def create
     Post.create(post_params)
+    redirect_to action: :index
   end
   
   private
